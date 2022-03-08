69f5ea8694038df926f7e59dda5ca2e9f865118f
Henning Schwichow <97026408+hennifant@users.noreply.github.com>
2022-03-08 19:30:21 +0100

    Update FishCard.spec.js

--- src/components/FishCard.spec.js
+++ src/components/FishCard.spec.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import DrinkCard from './FishCard';
+import FishCard from './FishCard';
 
 describe('FishCard', () => {
   it('render a div with heading and list of fish', () => {
